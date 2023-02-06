dc39d0cf37060ad784120bd831da484d63de9fda
abel <abelw34@gmail.com>
2023-02-06 20:39:44 +0300

    fixed transfer funds route

--- controllers/account.js
+++ controllers/account.js
@@ -55,11 +55,34 @@ export const withdrawFunds = async (req, res) => {
 export const transferFunds = async (req, res) => {
   const { balance } = req.body;
   const { id } = req.params;
+  const { funds } = req.query;
   try {
-    //get the user to recieve funds
-    const sender = Account.findById(id);
+    //get the user to send funds
+    const sender = await Account.findById(id);
 
-    console.log(sender);
+    //deduct the balance
+    const newBal = sender.balance - balance;
+
+    //update the new balance
+    const updatedBal = await Account.updateOne(
+      { _id: id },
+      {
+        balance: newBal,
+      }
+    );
+
+    //get the account to recieve the funds
+
+    const reciever = await Account.findById(funds);
+
+    //update the reciever account balance
+    const recieverNewBal = await Account.updateOne(
+      { _id: funds },
+      {
+        $inc: { balance: balance },
+      }
+    );
+    res.status(200).json({ recieverNewBal, updatedBal });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
